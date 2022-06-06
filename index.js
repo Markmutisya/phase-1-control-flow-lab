8ea29498b48863f1be020c7171a37734e9f1b337
Markmutisya <mark.mwangi@dtudent.moringaschool.com>
2022-06-06 21:50:12 +0300

    Control Flow

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
+  if (feet<= 400)
+  return 'This is on me!';
   // Write your code here!
+} else if (1999<feet&&feet<2500) {
+  return "I will gladly take your thirty bucks.";
+}else{
+  return "No can do";
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity (city) {
   // Write your code here!
+  return city == 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip (tip) {
   // Write your code here!
+  switch (tip)
+  case 'generous':
 }
